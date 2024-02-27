0c1f5aa10222605ddeae0db29592df402713ca39
Rodion Kotov <r.kotov@rodkot.site>
2024-02-27 17:56:39 +0700

    [Sniffer T3](#6) Добавил на страницу Home секундамер и кнопки старт/стоп

--- App/View/Pages/HomePage.xaml.cs
+++ App/View/Pages/HomePage.xaml.cs
@@ -7,5 +7,6 @@ public partial class HomePage : ContentPage
     public HomePage(HomeViewModel viewModel)
     {
         InitializeComponent();
+        BindingContext = viewModel;
     }
 }
--- App/ViewModels/HomeViewModel.cs
+++ App/ViewModels/HomeViewModel.cs
@@ -1,6 +1,69 @@
-﻿namespace Sniffer.ViewModels;
+﻿using System.Diagnostics;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
-public class HomeViewModel : AViewModel
+namespace Sniffer.ViewModels;
+
+public partial class HomeViewModel : AViewModel
 {
-   
+    private bool _isTimerRunning;
+    
+    [ObservableProperty]
+    private Stopwatch _stopwatch = new();
+
+    [ObservableProperty]
+    private bool _isStartButtonEnabled = true;
+    
+    [ObservableProperty]
+    private bool _isStopButtonEnabled;
+
+    
+    private string _timerText = "00:00:000";
+    public string TimerText
+    {
+        get => _timerText;
+        private set
+        {
+            if (_timerText == value) return;
+            _timerText = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    [RelayCommand]
+    private void StartSniffer()
+    {
+        _isTimerRunning = true;
+        IsStartButtonEnabled = false;
+        IsStopButtonEnabled = true;
+        
+        Stopwatch.Restart();
+        Application.Current?.Dispatcher.StartTimer(TimeSpan.FromMilliseconds(100), UpdateTimer);
+    }
+
+    [RelayCommand]
+    private void StopSniffer()
+    {
+        _isTimerRunning = false;
+        IsStartButtonEnabled = true;
+        IsStopButtonEnabled = false;
+    }
+    
+
+     
+    private bool UpdateTimer()
+    {
+        if (_isTimerRunning)
+        {
+            Application.Current?.Dispatcher.Dispatch(UpdateTimerText);
+        }
+
+        return _isTimerRunning;
+    }
+
+    private void UpdateTimerText()
+    {
+        var elapsed = Stopwatch.Elapsed;
+        TimerText = $"{elapsed.Minutes:D2}:{elapsed.Seconds:D2}:{elapsed.Milliseconds:D3}";
+    }
 }
