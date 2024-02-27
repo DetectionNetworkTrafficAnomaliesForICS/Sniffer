bc5b64abd7ce06688dacc48c42a616105c92e47f
Rodion Kotov <r.kotov@rodkot.site>
2024-02-27 14:56:37 +0700

    [Sniffer T2](#5) Добавил репозиторий для работы с настройками, храняющихся в хэш

---
+++ App/Repositories/IPreferenceRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Sniffer.Repositories;
+
+public interface IPreferenceRepository
+{
+    public void Set<T>(string key, T value);
+    public T Get<T>(string key, T defValue);
+}
---
+++ App/Repositories/Impl/PreferenceRepositoryImpl.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace Sniffer.Repositories.Impl;
+
+public class PreferenceRepositoryImpl : IPreferenceRepository
+{
+    public void Set<T>(string key, T value)
+    {
+        var json = JsonSerializer.Serialize(value);
+        Preferences.Default.Set(key, json);
+    }
+
+    public T Get<T>(string key, T defValue)
+    {
+        var json = Preferences.Default.Get(key, string.Empty);
+        return json.Equals(string.Empty) ? defValue : JsonSerializer.Deserialize<T>(json);
+    }
+}
