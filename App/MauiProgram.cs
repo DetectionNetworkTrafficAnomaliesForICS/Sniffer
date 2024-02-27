00ed5eed472641a5b64921efeb3e653c9330408b
Rodion Kotov <r.kotov@rodkot.site>
2024-02-27 14:59:23 +0700

    [Sniffer T2](#5) * Добавил в UI интерфейс возможность настраивать параметры директории и сетевого интерфейса. * Настройка DI контейнеров для сервисов и репозиториев

--- App/MauiProgram.cs
+++ App/MauiProgram.cs
@@ -1,6 +1,10 @@
 ﻿using CommunityToolkit.Maui;
 using Microsoft.Extensions.Logging;
 using Microsoft.Maui.LifecycleEvents;
+using Sniffer.Repositories;
+using Sniffer.Repositories.Impl;
+using Sniffer.Services;
+using Sniffer.Services.Impl;
 using Sniffer.View.Pages;
 using Sniffer.ViewModels;
 
@@ -21,6 +25,8 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
         
+        _AddRepositories(services);
+        _AddServices(services);
         _AddPageAndViewModelServices(services);
 
 #if DEBUG
@@ -28,6 +34,20 @@ public static class MauiProgram
 #endif
 
         return builder.Build();
+    }
+
+    private static void _AddServices(IServiceCollection services)
+    {
+        services.AddSingleton<ISettingsService,SettingsServiceImpl>();
+        services.AddSingleton<IDirectoryService,DirectoryServiceImpl>();
+        services.AddSingleton<INetService,NetInterfaceServiceImpl>();
+    }
+
+    private static void _AddRepositories(IServiceCollection services)
+    {
+        services.AddSingleton<INetInterfaceRepository,NetInterfaceRepositoryImpl>();
+        services.AddSingleton<IDirectoryRepository,DirectoryRepositoryImpl>();
+        services.AddSingleton<IPreferenceRepository,PreferenceRepositoryImpl>();
     }
 
     private static void _AddPageAndViewModelServices(IServiceCollection services)
