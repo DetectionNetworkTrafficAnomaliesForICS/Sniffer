5ce9415a591fca9725c4e986b1fb459b15484cb2
Rodion Kotov <r.kotov@rodkot.site>
2024-02-27 14:57:21 +0700

    [Sniffer T2](#5) Добавил сервис настроек

---
+++ App/Services/ISettingsService.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using Sniffer.Models;
+using Directory = Sniffer.Models.Directory;
+
+namespace Sniffer.Services;
+
+public interface ISettingsService : INotifyPropertyChanged
+{
+    public Directory TrafficDirectory { get; set; }
+    public NetInterface NetInterface { get; set; }
+}
---
+++ App/Services/Impl/SettingsServiceImpl.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using Sniffer.Models;
+using Sniffer.Repositories;
+using Directory = Sniffer.Models.Directory;
+
+namespace Sniffer.Services.Impl;
+
+public sealed class SettingsServiceImpl : ISettingsService
+{
+    private readonly INetInterfaceRepository _netInterfaceRepository;
+    private readonly IDirectoryRepository _directoryRepository;
+    private readonly IPreferenceRepository _preferenceRepository;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public Directory TrafficDirectory
+    {
+        get => _preferenceRepository.Get(nameof(TrafficDirectory), _directoryRepository.GetDefaultDirectory());
+        set => _preferenceRepository.Set(nameof(TrafficDirectory), value);
+    }
+
+    public NetInterface NetInterface 
+    {
+        get => _preferenceRepository.Get(nameof(NetInterface), _netInterfaceRepository.GetDefaultGateway());
+        set => _preferenceRepository.Set(nameof(NetInterface), value);
+    }
+
+    public SettingsServiceImpl(INetInterfaceRepository netInterfaceRepository, IDirectoryRepository directoryRepository,
+        IPreferenceRepository preferenceRepository)
+    {
+        _netInterfaceRepository = netInterfaceRepository;
+        _directoryRepository = directoryRepository;
+        _preferenceRepository = preferenceRepository;
+        
+    }
+}
