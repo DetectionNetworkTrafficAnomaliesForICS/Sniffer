38f14bb9abd93fc58f0c24c7a1f16a0ba45059d0
Rodion Kotov <r.kotov@rodkot.site>
2024-02-28 12:40:55 +0700

    [Sniffer B1](#10) Добавил реализацию абстракций сервисов и репозиториев для .Net Core

---
+++ Sniffer.Core/Repositories/Impl/PreferenceRepositoryImpl.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+
+namespace Sniffer.Core.Repositories.Impl;
+
+public class PreferenceRepositoryImpl : IPreferenceRepository
+{
+    private readonly Dictionary<string, string> _dictionary = new();
+
+    public void Set<T>(string key, T value)
+    {
+        var json = JsonSerializer.Serialize(value);
+        _dictionary[key] = json;
+    }
+
+    public T Get<T>(string key, T defValue)
+    {
+        if (_dictionary.TryGetValue(key, out var json))
+        {
+            return JsonSerializer.Deserialize<T>(json) ?? defValue;
+        }
+
+        return defValue;
+    }
+}
