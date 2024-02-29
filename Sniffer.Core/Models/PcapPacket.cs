a2a37101c6332f9d2f369bcfefae27fc67b4c875
Rodion Kotov <r.kotov@rodkot.site>
2024-03-01 00:43:07 +0700

    [Sniffer T3](#6) Добавление интерфейса модели пакетов и его реализации для SharpPcap

---
+++ Sniffer.Core/Models/PcapPacket.cs
@@ -0,0 +1,29 @@
+﻿using PacketDotNet;
+using SharpPcap;
+using Sniffer.Lib.Models;
+
+namespace Sniffer.Core.Models;
+
+public class PcapPacket : INetPacket
+{
+    public PcapPacket(PacketCapture packetCapture)
+    {
+        var rawPacket = packetCapture.GetPacket();
+
+        var packet = Packet.ParsePacket(rawPacket.LinkLayerType, rawPacket.Data);
+
+        var ethernetPacket = packet.Extract<EthernetPacket>();
+        var ipPacket = packet.Extract<IPPacket>();
+        var tcpPacket = packet.Extract<TcpPacket>();
+
+        SourceDevice = new INetPacket.Device(tcpPacket.SourcePort, ethernetPacket.SourceHardwareAddress.ToString(),
+            ipPacket.SourceAddress.ToString());
+        DestinationDevice = new INetPacket.Device(tcpPacket.DestinationPort, ipPacket.DestinationAddress.ToString(),
+            ethernetPacket.DestinationHardwareAddress.ToString());
+        Data = tcpPacket.PayloadData;
+    }
+
+    public INetPacket.Device SourceDevice { get; }
+    public INetPacket.Device DestinationDevice { get; }
+    public byte[] Data { get; }
+}
---
+++ Sniffer.Lib/Models/INetPacket.cs
@@ -0,0 +1,11 @@
+﻿namespace Sniffer.Lib.Models;
+
+public interface INetPacket
+{
+    public Device SourceDevice { get; }
+    public Device DestinationDevice { get; }
+
+    public byte[] Data { get; }
+
+    public record Device(int Port, string MacAddress, string IpAddress);
+}
