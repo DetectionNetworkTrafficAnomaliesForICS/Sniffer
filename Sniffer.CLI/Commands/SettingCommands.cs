5e0ffad7bd31e2ace44fbc0ba27e5a9f144bb178
Rodion Kotov <r.kotov@rodkot.site>
2024-03-01 00:46:55 +0700

    [Sniffer T3](#6) Добавил интераетацию команды настройки сетевого интерфейса

--- Sniffer.CLI/Commands/SettingCommands.cs
+++ Sniffer.CLI/Commands/SettingCommands.cs
@@ -6,10 +6,12 @@ namespace Sniffer.CLI.Commands;
 public class SettingCommands
 {
     private readonly ISettingsService _settingsService;
+    private readonly INetService _netService;
 
-    public SettingCommands(ISettingsService settingsService)
+    public SettingCommands(ISettingsService settingsService, INetService netService)
     {
         _settingsService = settingsService;
+        _netService = netService;
     }
 
     public void Run()
@@ -30,7 +32,46 @@ public class SettingCommands
                     SettingFolder();
                     break;
                 case "2":
+                    SettingDevice();
+                    break;
+                case "3":
+                    Console.WriteLine("Exiting Setting.");
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice!");
+                    break;
+            }
+        }
+    }
 
+    private void SettingDevice()
+    {
+        while (true)
+        {
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine(">>>Menu Net Device:");
+            Console.WriteLine("1. Read");
+            Console.WriteLine("2. ReChoose");
+            Console.WriteLine("3. Exit");
+            Console.Write(">>>Enter your choice: ");
+            var choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine(_settingsService.NetDevice);
+                    break;
+                case "2":
+                    var devices = _netService.GetAll();
+                    for (var i = 0; i < devices.Count; i++)
+                    {
+                        Console.WriteLine($"{i}. {devices[i]}");
+                    }
+                    
+                    var input = Console.ReadLine();
+                    if (int.TryParse(input, out var index))
+                    {
+                        _settingsService.NetDevice = devices[index];
+                    }
                     break;
                 case "3":
                     Console.WriteLine("Exiting Setting.");
