bb02d4621637240afa0d340ada2eb68987dbb104
Rodion Kotov <r.kotov@rodkot.site>
2024-03-30 23:07:12 +0700

    [Sniffer T4](#20) Исправил неправильный порядок байтов в парсере Modbus пакетов

--- Sniffer.Core/Services/Impl/ModbusServiceImpl.cs
+++ Sniffer.Core/Services/Impl/ModbusServiceImpl.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Sniffer.Core.Models;
 using Sniffer.Lib.Models;
 using Sniffer.Lib.Services.Interfaces;
@@ -9,15 +10,15 @@ public class ModbusServiceImpl : IModbusService
 {
     public bool TryConvertToModbusPacket(INetPacket netPacket, out IModbusPacket? result)
     {
-        var bytes = netPacket.Data;
+        var bytes = netPacket.Data.Reverse().ToArray();
         try
         {
-            var tranId = BitConverter.ToUInt16(bytes, 0);
-            var protocolId = BitConverter.ToUInt16(bytes, 2);
-            var len = BitConverter.ToUInt16(bytes, 4);
-            var deviceId = bytes[6];
+            var tranId = BitConverter.ToUInt16(bytes, bytes.Length - 2);
+            var protocolId = BitConverter.ToUInt16(bytes, bytes.Length - 4);
+            var len = BitConverter.ToUInt16(bytes, bytes.Length - 6);
+            var deviceId = bytes[bytes.Length - 7];
             var pdu = new byte[bytes.Length - 7];
-            Array.Copy(bytes, 7, pdu, 0, bytes.Length - 7);
+            Array.Copy(bytes, 0, pdu, 0, bytes.Length - 7);
 
             result = new ModbusPacket(tranId, protocolId, len, deviceId, pdu);
             return true;
